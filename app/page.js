9c30e69524944994cdfc39f3bc5a1d447fe9aca1
root <root@kali>
2026-09-22 01:33:29 +0300

    Update prices, burger fix, and remove fixed delivery fea

--- app/page.js
+++ app/page.js
@@ -95,7 +95,7 @@ export default function MenuPage() {
       { name: 'اقاشي لحمة', img: 'https://images.unsplash.com/photo-1544025162-d76694265947?w=500&auto=format&fit=crop&q=60', prices: { سوري: '100 ج', فرنساوي: '110 ج' } },
       { name: 'كرسبي', img: 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?w=500&auto=format&fit=crop&q=60', prices: { سوري: '90 ج', فرنساوي: '100 ج' } },
       { name: 'شيش طاووق', img: 'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=500&auto=format&fit=crop&q=60', prices: { سوري: '100 ج', فرنساوي: '110 ج' } },
-      { name: 'بيرقر', price: 100, img: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd'},
+      { name: 'بيرقر', img: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd', prices: { 'عادي': 100 } },
 
     ],
     mojito: [
